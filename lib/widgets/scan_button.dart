f7582ee4083b1d207b2a652c2f7f1d33ab1cfd1d
Nacho <44799327+nachom96@users.noreply.github.com>
2022-08-03 16:31:16 +0200

    Añadido lector código QR (Falta funcionalidad)

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
 class ScanButton extends StatelessWidget {
   const ScanButton({Key? key}) : super(key: key);
 
@@ -7,7 +9,10 @@ class ScanButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       elevation: 0,
-      onPressed: (){},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancelar', false , ScanMode.QR);
+        print(barcodeScanRes);
+      },
       child: const Icon(Icons.filter_center_focus),
       );
   }
