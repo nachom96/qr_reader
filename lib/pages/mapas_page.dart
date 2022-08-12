2b1b1a362946b568b45f1349b07c3e45499c5360
Nacho <nacho_085@hotmail.com>
2022-08-12 13:35:09 +0200

    Corregido error Dismissible

--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -14,12 +14,21 @@ class MapasPage extends StatelessWidget {
 
     return ListView.builder(
       itemCount: scans.length,
-      itemBuilder: (_, i) => ListTile(
-        leading: Icon(Icons.map, color: Theme.of(context).primaryColor,),
-        title: Text(scans[i]!.valor),
-        subtitle: Text(scans[i]!.id.toString()),
-        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey,),
-        onTap: () => print('Abrir algo'),
+      itemBuilder: (_, i) => Dismissible(
+        key: UniqueKey(),
+        background: Container(
+          color: Colors.red,
+        ),
+        onDismissed: (DismissDirection direction){
+          Provider.of<ScanListProvider>(context, listen: false).borrarScanPorId(scans[i]!.id);
+        },
+        child: ListTile(
+          leading: Icon(Icons.map, color: Theme.of(context).primaryColor,),
+          title: Text(scans[i]!.valor),
+          subtitle: Text(scans[i]!.id.toString()),
+          trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey,),
+          onTap: () => print('Abrir algo'),
+        ),
       )
     );
   }
