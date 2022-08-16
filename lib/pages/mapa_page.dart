faf5c25b1efcf76304867946780f7df78a22bc27
Nacho <nacho_085@hotmail.com>
2022-08-16 20:19:39 +0200

    Añadido mapa normal y mapa satélite

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -14,6 +14,8 @@ class MapaPage extends StatefulWidget {
 class _MapaPageState extends State<MapaPage> {
   Completer<GoogleMapController> _controller = Completer();
 
+  MapType mapType = MapType.normal;
+
   @override
   Widget build(BuildContext context) {
 
@@ -25,16 +27,56 @@ class _MapaPageState extends State<MapaPage> {
       tilt: 40
     );
 
+    // Marcadores
+    Set<Marker> markers = Set<Marker>();
+    markers.add(Marker(
+      markerId: MarkerId('geo-location'),
+      position: scan.getLatLng()
+
+    ));
+
+
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapa'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.location_on_rounded),
+            onPressed: () async{
+              final GoogleMapController controller = await _controller.future;
+              controller.animateCamera(
+                CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                    target: scan.getLatLng(),
+                    zoom: 17,
+                    tilt: 40
+                  )
+                )
+                );
+
+            }, 
+            ),
+        ],
       ),
       body: GoogleMap(
-        mapType: MapType.normal,
+        mapType: mapType,
+        markers: markers,
         initialCameraPosition: puntoInicial,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.layers),
+        onPressed: (){
+          if ( mapType == MapType.normal){
+            mapType = MapType.satellite;
+          } else{
+            mapType = MapType.normal;
+          }
+
+          setState(() {});
         },
       ),
     );
