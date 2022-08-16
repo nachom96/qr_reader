ddb2778f6ab568f893bb57a1e290d0a224b5d1a2
Nacho <nacho_085@hotmail.com>
2022-08-16 19:30:48 +0200

    Añadido mapa al tocar el botón central

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,19 +1,39 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qr_reader/providers/db_provider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
   const MapaPage({Key? key}) : super(key: key);
 
   @override
+  State<MapaPage> createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
   Widget build(BuildContext context) {
-    final ScanModel scan = ModalRoute.of(context)?.settings.arguments as ScanModel;
+    final CameraPosition puntoInicial = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
+
+    final ScanModel scan =
+        ModalRoute.of(context)?.settings.arguments as ScanModel;
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapa'),
       ),
-      body: Center(
-        child: Text(scan.valor),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
